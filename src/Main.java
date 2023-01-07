f7d87101e68ed88921f58777054ff8ce6976dfc7
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2023-01-07 12:09:04 +0200

    Created Main Clas with Random object

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -8,5 +9,28 @@ public class Main {
         int slots = 6;
         // all elements are initialized to be zeros
         int[] lotteryArray = new int[slots];
+        boolean isRepeated;
+        int randomNumber = 0;
+        for (int indexDrawn = 0; indexDrawn < slots; indexDrawn++) {
+            do {
+                isRepeated = false;
+                // number between 1 and 49
+                randomNumber = rand.nextInt(max + 1 - min) + min;
+                //check for repeated number against the filled non-zero elements
+                for (int k = 0; k <= indexDrawn; k++) {
+                    //check against each filled non-zero elements. Hope that none repeats the random number
+                    if (lotteryArray[k] == randomNumber) {
+                        isRepeated = true;
+                        break;
+                    }
+                }
+            } while (isRepeated);
+            lotteryArray[indexDrawn] = randomNumber;
+        }
+        Arrays.sort(lotteryArray);
+        System.out.println("The result is:  ");
+        for (int i = 0; i < slots; i++) {
+            System.out.print(lotteryArray[i] + " ");
+        }
     }
 }
