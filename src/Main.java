2a81e1ce4f26e4b907f786882af2f3df07f9105a
NikoDimitrov1991 <ndnikolaydimitrov@gmail.com>
2023-01-07 11:31:41 +0200

    Created Main Clas with Random object

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int min = 1;
+        int max = 49;
+        int slots = 6;
+    }
+}
